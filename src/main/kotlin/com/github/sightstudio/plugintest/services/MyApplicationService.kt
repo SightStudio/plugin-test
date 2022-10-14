7284f9d5f88985b424139ebdb22a2d55d57fb9bb
GitHub Action <action@github.com>
2022-10-14 19:27:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/sightstudio/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sightstudio/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sightstudio.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sightstudio.plugintest.MyBundle
 
 class MyApplicationService {
 
